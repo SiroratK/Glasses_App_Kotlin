16631717a5e130d2a3fa203a1669634d9a6ad07d
sirorat <sirorattungo@gmail.com>
2020-02-13 02:45:53 +0700

    Edit to detect image no live camera

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,46 +1,22 @@
 package com.example.myapplication
 
-
-import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    val drawerToggle by lazy {
-        ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drewer_close)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        drawerLayout.addDrawerListener(drawerToggle)
-        val fragment = PreviewFragment.newInstance()
-        replaceFragment(fragment)
-
     }
-
-    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onPostCreate(savedInstanceState, persistentState)
-        drawerToggle.syncState()
+    fun faceDetection(view: View){
+        startActivity(Intent(this@MainActivity, FaceDetection::class.java))
     }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        drawerToggle.onConfigurationChanged(newConfig)
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
-        fragmentTransaction.commit()
-    }
-
 }
 
 
