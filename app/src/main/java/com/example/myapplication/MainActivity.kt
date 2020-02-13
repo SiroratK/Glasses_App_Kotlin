d5e78af04c88600aedca46c98aa69169cdcc982c
sirorat <sirorattungo@gmail.com>
2020-02-13 18:15:34 +0700

    Revert "Edit camera API to camera2"
    This reverts commit 1bd49305c5b201cc3dbc6810a8886a7c6a45fcfb.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,14 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSION_REQUEST_CODE = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
     }
     fun faceDetection(view: View){
         startActivity(Intent(this@MainActivity, FaceDetection::class.java))
     }
 }
-
 
